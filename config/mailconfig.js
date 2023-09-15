a5beffa0f8e83e8cc04c589e21ca85636985c477
Ssilith <ktrina010@gmail.com>
2023-09-15 22:57:00 +0200

    mailtrap

--- config/mailconfig.js
+++ config/mailconfig.js
@@ -1,20 +1,20 @@
-// if (process.env.NODE_ENV === "development") {
-// module.exports = {
-//   host: "sandbox.smtp.mailtrap.io",
-//   port: "587",
-//   auth: {
-//     user: "a03467227a2d95",
-//     pass: "60d2eb2f64e68c",
-//   },
-// };
-// } else {
-module.exports = {
-  host: "smtp.gmail.com",
-  // port: "587",
-  port: "465",
-  auth: {
-    user: "moneytracker.biuro@gmail.com",
-    pass: process.env.MAILER_PSW,
-  },
-};
-// }
+if (process.env.NODE_ENV === "development") {
+  module.exports = {
+    host: "sandbox.smtp.mailtrap.io",
+    port: "587",
+    auth: {
+      user: "a03467227a2d95",
+      pass: "60d2eb2f64e68c",
+    },
+  };
+} else {
+  module.exports = {
+    host: "smtp.gmail.com",
+    // port: "587",
+    port: "465",
+    auth: {
+      user: "moneytracker.biuro@gmail.com",
+      pass: process.env.MAILER_PSW,
+    },
+  };
+}
