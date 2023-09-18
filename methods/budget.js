4450a44fcb440608a38c10cb6288b43ff86dcf4a
Ssilith <ktrina010@gmail.com>
2023-09-18 18:02:02 +0200

    setbudget overlap

--- methods/budget.js
+++ methods/budget.js
@@ -2,8 +2,34 @@ const Budget = require("../models/budget");
 const User = require("../models/user");
 
 var functions = {
+    // addNewBudget: async function (req, res) {
+    //     try {
+    //         let newBudget = new Budget(req.body.budget);
+    //         newBudget = await newBudget.save();
+    //         console.log(newBudget);
+    //         return res.status(200).send({ success: true, budget: newBudget });
+    //     } catch (e) {
+    //         console.log(e);
+    //         return res.status(500).send({ success: false, msg: e });
+    //     }
+    // },
+
     addNewBudget: async function (req, res) {
         try {
+            let { startDate, endDate } = req.body.budget;
+
+            let overlappingBudgets = await Budget.find({
+                $or: [
+                    { startDate: { $lte: startDate }, endDate: { $gte: startDate } },
+                    { startDate: { $lte: endDate }, endDate: { $gte: endDate } },
+                    { startDate: { $gte: startDate }, endDate: { $lte: endDate } }
+                ]
+            });
+
+            if (overlappingBudgets.length > 0) {
+                return res.status(400).send({ success: false, msg: 'budgetOverlaps' });
+            }
+
             let newBudget = new Budget(req.body.budget);
             newBudget = await newBudget.save();
             console.log(newBudget);
@@ -13,6 +39,7 @@ var functions = {
             return res.status(500).send({ success: false, msg: e });
         }
     },
+
 
     updateBudget: async function (req, res) {
         try {
