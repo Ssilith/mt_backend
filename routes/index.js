ed1ba3638039ffac1a7e1c294180803c934ef9d8
Ssilith <ktrina010@gmail.com>
2023-10-02 20:37:41 +0200

    code cleaning

--- routes/index.js
+++ routes/index.js
@@ -7,53 +7,30 @@ const budget = require("../methods/budget").functions;
 const transaction = require("../methods/transaction").functions;
 const category = require("../methods/category").functions;
 
+//##########################		GENERAL		##########################
 router.post("/cleardb", general.cleardb);
 
 //##########################		USER		##########################
-
-//@desc Adding a new user
-//@route POST /adduser
 router.post("/addUser", user.addNew);
 
-//@desc Authenticate a user
-//@route POST /authenticate
 router.post("/authenticate", user.authenticate);
 
-//@desc Confirm the identity of the user by verifying his credentials,
-//if correct return short-lived jwt to be used in request that requires identity confirmation
-//@route POST /confirmIdentity
 router.post("/confirmIdentity", verify, user.confirmIdentity);
 
-//@desc Check current access token, if expired generate new access token using refresh token
-//@route POST /refreshToken
 router.post("/refreshToken", user.refreshToken);
 
-//@desc Logout user
-//@route DELETE /logout
 router.delete("/logout", user.logout);
 
-//@desc Get user informations
-//@route GET /getinfo
 router.get("/getUserInfo", verify, user.getUserInfo);
 
-//@desc Get a list of all users sorted alphabetically by username, to be used in admin panel
-//@route GET /getUsersForAdmin
 router.get("/getUsersForAdmin", verify, user.getUsersForAdmin);
 
-//@desc Update user anagraphic informations
-//@route POST /updateUserInfo
 router.post("/updateUserInfo", verify, user.updateUserInfo);
 
-//@desc Send password reset email
-//@route POST /sendResetPwdEmail
 router.post("/sendResetPwdEmail", user.sendResetPwdEmail);
 
-//@desc Render Reset password page
-//@route POST /resetPasswordRender
 router.get("/resetPasswordRender", user.resetPasswordRender);
 
-//@desc Reset password
-//@route POST /resetPassword
 router.post("/resetPassword", user.resetPassword);
 
 router.post("/getUserId", user.getUserId);
